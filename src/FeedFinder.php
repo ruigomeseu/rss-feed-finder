b07faf53ae57685b154d99a1b6fe6452a61330a5
Rui Gomes <hello@ruigomes.me>
2016-09-15 13:24:20 +0100

    Initial commit

---
+++ src/FeedFinder.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace RuiGomes;
+
+use Illuminate\Support\Collection;
+use PHPHtmlParser\Dom;
+
+class FeedFinder
+{
+    /**
+     * @var \PHPHtmlParser\Dom
+     */
+    protected $dom;
+
+    /**
+     * @var string
+     */
+    protected $url;
+
+    protected $feedTypes = [
+        'application/rss+xml',
+        'application/atom+xml',
+    ];
+
+    public function __construct($url)
+    {
+        $this->dom = new Dom;
+        $this->url = $url;
+    }
+
+    public function setUrl($url)
+    {
+        $this->url = $url;
+    }
+
+    public function find()
+    {
+        $this->dom->loadFromUrl($this->url);
+
+        $feeds = new Collection($this->dom->find('link[rel=alternate]'));
+
+        $feedUrls = $feeds->filter(function ($feed) {
+            return in_array($feed->getAttribute('type'), $this->feedTypes);
+        })->map(function($feed) {
+            return $feed->getAttribute('href');
+        })->toArray();
+
+        return $feedUrls;
+    }
+}
